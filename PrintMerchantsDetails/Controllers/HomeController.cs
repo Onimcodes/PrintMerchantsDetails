fc9d3884ad8a02a44f8554c1f4f8123dc27d1853
Onimcodes <Abrahammicheal55@yahoo.com>
2022-09-13 09:39:39 +0100

    Add project files.

---
+++ PrintMerchantsDetails/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PrintMerchantsDetails.Models;
+
+namespace PrintMerchantsDetails.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            MerchantTransactionDetails transactionDetails = new MerchantTransactionDetails()
+            {
+                MerchantName = "lIZPEE GLOBAL RESOURCES",
+                Location = "NO. 74 NTA/MBUORA RD, PORTHARCOURT",
+                TerminalId = 23011845,
+                STAN = 003862,
+                DateTime = DateTime.Parse("2022-08-30 17:32:58"),
+                Amount = 0.01,
+                AID = "A0000000041010",
+                ExpiryDate = DateTime.Parse("11/24"),
+                AuthorizationCode = "000000",
+                DebitCard = "539941******3369",
+                PTAD = "Global Accelerex",
+                RRN = 000210002450,
+                ResponseCode = 91
+
+            };   
+            
+            return View(transactionDetails);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PrintMerchantsDetails/Models/MerchantTransactionDetails.cs
@@ -0,0 +1,21 @@
+﻿namespace PrintMerchantsDetails.Models
+{
+    public class MerchantTransactionDetails
+    {
+        public string MerchantName { get; set; } = default!;
+        public string Location { get; set; } = default!;
+        public int TerminalId { get; set; } = default!;
+        public int STAN { get; set; } = default!;  
+        public DateTime DateTime { get; set; } = default!;
+        public double Amount { get; set; } 
+        public string DebitCard { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string AuthorizationCode { get; set; }
+        public int ResponseCode { get; set; }
+        public string AID { get; set; }
+        public int RRN { get; set; }
+        public string PTAD { get; set; }
+
+
+    }
+}
